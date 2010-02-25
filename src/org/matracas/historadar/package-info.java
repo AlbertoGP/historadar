ab39982a5b7adfed3324da15a65a3e4ca6fb2520
Alberto Gonz?lez Palomo <Alberto.Gonzalez@matracas.org>
2010-02-25 11:47:49 +0100

    Corrected the project name in some places where it still had the old one, and added a copy of the GPL license.

--- src/org/matracas/historadar/package-info.java
+++ src/org/matracas/historadar/package-info.java
@@ -10,13 +10,13 @@
 //   the Free Software Foundation; either version 3 of the License, or
 //   (at your option) any later version.
 //
-//   JFT is distributed in the hope that it will be useful,
+//   HistoRadar is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
 //
 //   You should have received a copy of the GNU General Public License
-//   along with JFT; if not, see <http://www.gnu.org/licenses/>.
+//   along with HistoRadar; if not, see <http://www.gnu.org/licenses/>.
 //
 /////////////////////////////////////////////////////////////////////////////
 
