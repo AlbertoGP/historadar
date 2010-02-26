d71d228d4afff21a08af735d7c54ee4f46730c60
Johannes Braunias <johannes.braunias@gmail.com>
2010-02-26 19:06:55 +0100

    Trying out regular expressions on the text, but not working

---
+++ src/org/matracas/historadar/View.java
@@ -0,0 +1,154 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2010 Alberto González Palomo
+//   Author: Alberto González Palomo - http://matracas.org/
+//
+//   This file is part of HistoRadar, the History Radar.
+//
+//   HistoRadar is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   HistoRadar is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with HistoRadar; if not, see <http://www.gnu.org/licenses/>.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.matracas.historadar;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import org.matracas.historadar.Document;
+import org.matracas.historadar.nlp.OCR;
+import org.matracas.historadar.nlp.Metadata;
+
+/**
+ * Main class of HistoRadar, with the GUI application.
+ */
+public class View implements ActionListener
+{
+    private JFrame window;
+    private JTextPane documentView;
+    private HeatMap heatMap;
+    private JPanel metadataPane;
+    
+    public View()
+    {
+        JFrame.setDefaultLookAndFeelDecorated(false);
+        window = new JFrame("HistoRadar View");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800,600);
+        buildGUI(window);
+        window.setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        String command = e.getActionCommand();
+        if ("quit".equals(command)) {
+            System.exit(0);
+        }
+        else if ("load-collection".equals(command)) {
+            String lastCollectionLoaded = System.getProperty("historadar.defaultCollection");
+            if (null == lastCollectionLoaded) lastCollectionLoaded = ".";
+            JFileChooser fileChooser = new JFileChooser(lastCollectionLoaded);
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int returnValue = fileChooser.showOpenDialog(window);
+            if (JFileChooser.APPROVE_OPTION == returnValue) {
+                File directory = fileChooser.getSelectedFile();
+                System.setProperty("historadar.defaultCollection", directory.getAbsolutePath());
+                Document.Collection documents = new Document.Collection(directory);
+                Document document = documents.getDocumentIterator().next();
+                
+                // OCR correction
+                OCR ocr = new OCR(documents);
+                int corrections = ocr.correctDocument(document);
+                System.err.println("Corrected " + corrections + " errors from the OCR text");
+                documentView.setText(document.getPlainText());
+                
+                // Metadata extraction
+                Metadata metadata = new Metadata(documents);
+                Metadata.Entries entries = metadata.getMetadata(document);
+                String value;
+                metadataPane.removeAll();
+                value = entries.title();
+                System.err.println("title=" + value);
+                if (value != null) metadataPane.add(new JLabel("Title: " + value));
+                value = entries.date();
+                System.err.println("date=" + value);
+                if (value != null) metadataPane.add(new JLabel("Date: " + value));
+                window.validate();
+            }
+            else {
+                System.err.println("Collection loading cancelled");
+            }
+        }
+        else {
+            System.err.println("Error: Unexpected command: '" + command + "'");
+            System.exit(1);
+        }
+    }
+    
+    private void buildGUI(JFrame frame)
+    {
+        documentView = new JTextPane();
+        documentView.setEditable(false);
+        documentView.setContentType("text/plain");
+        
+        heatMap = new HeatMap();
+        heatMap.addActionListener(this);
+        heatMap.setActionCommand("heat-map-click");
+        
+        metadataPane = new JPanel();
+        JPanel documentPane;
+        JScrollPane heatMapPane;
+        documentPane = new JPanel();
+        heatMapPane = new JScrollPane(heatMap);
+        
+        documentPane.setLayout(new BoxLayout(documentPane, BoxLayout.PAGE_AXIS));
+        metadataPane.setLayout(new BoxLayout(metadataPane, BoxLayout.PAGE_AXIS));
+        documentPane.add(metadataPane);
+        documentPane.add(new JScrollPane(documentView));
+        
+        JSplitPane view = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, documentPane, heatMapPane);
+        view.setOneTouchExpandable(true);
+        view.setResizeWeight(0.8);
+        
+        frame.getContentPane().add(view);
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+        JMenuItem item;
+        menu.add(item = new JMenuItem("Load collection"));
+        item.addActionListener(this);
+        item.setActionCommand("load-collection");
+        menu.addSeparator();
+        menu.add(item = new JMenuItem("Quit"));
+        item.addActionListener(this);
+        item.setActionCommand("quit");
+        frame.setJMenuBar(menuBar);
+    }
+    
+    public static void main(String[] args)
+    {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() 
+                {
+                    View view = new View();
+                }
+            });
+    }
+}
---
+++ src/org/matracas/historadar/nlp/NER.java
@@ -0,0 +1,138 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2010 Alberto González Palomo
+//   Author: Alberto González Palomo - http://matracas.org/
+//
+//   This file is part of HistoRadar, the History Radar.
+//
+//   HistoRadar is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   HistoRadar is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with HistoRadar; if not, see <http://www.gnu.org/licenses/>.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.matracas.historadar.nlp;
+
+import java.util.Map;
+import java.util.Hashtable;
+import java.util.Vector;
+import org.matracas.historadar.Document;
+
+/**
+ * Named entities extracted from a document.
+ *
+ */
+public class NER
+{
+    private static final String NAMESPACE = "http://matracas.org/ns/historadar/";
+    
+    public NER(Document.Collection collection)
+    {
+        // TODO: linguistic analysis through the collection
+    }
+    
+    /**
+     * Get the entities from a given document,
+     * based on the linguistic of the document collection.
+     *
+     * @param document the document where we look for entities
+     */
+    public Entities getEntities(Document document)
+    {
+        Entities entities = new Entities();
+        String plainText = document.getPlainText();
+        
+        // TODO: extract entities form plain text
+        // These are some dumb examples until this is implemented:
+        entities.location("not here nor there");
+        entities.person("Big Kahuna");
+        entities.person("His Excellence Mister Foolserrand");
+        entities.person("Totem Master");
+        entities.organization("Council of Notable People");
+        
+        return entities;
+    }
+    
+    /**
+     * An entity extracted from a document, built from a string.
+     */
+    public static class Entity
+    {
+        protected String string;
+        
+        public Entity(String string)
+        {
+            this.string = string;
+        }
+        
+        /**
+         * Type of entity.
+         */
+        public enum Type
+        {
+            LOCATION, PERSON, ORGANIZATION;
+        }
+    }
+    
+    /**
+     * Table of entities indexed by type.
+     */
+    public class Entities extends Hashtable<Entity.Type, Vector<Entity> >
+    {
+        public Entities location(String location)
+        {
+            Vector<Entity> entitiesOfType = get(Entity.Type.LOCATION);
+            if (null == entitiesOfType) {
+                put(Entity.Type.LOCATION, entitiesOfType = new Vector<Entity>());
+            }
+            entitiesOfType.add(new Entity(location));
+            
+            return this;
+        }
+        public Vector<Entity> location()
+        {
+            return get(Entity.Type.LOCATION);
+        }
+        
+        public Entities person(String person)
+        {
+            Vector<Entity> entitiesOfType = get(Entity.Type.PERSON);
+            if (null == entitiesOfType) {
+                put(Entity.Type.PERSON, entitiesOfType = new Vector<Entity>());
+            }
+            entitiesOfType.add(new Entity(person));
+            
+            return this;
+        }
+        public Vector<Entity> person()
+        {
+            return get(Entity.Type.PERSON);
+        }
+        
+        public Entities organization(String organization)
+        {
+            Vector<Entity> entitiesOfType = get(Entity.Type.ORGANIZATION);
+            if (null == entitiesOfType) {
+                put(Entity.Type.ORGANIZATION, entitiesOfType = new Vector<Entity>());
+            }
+            entitiesOfType.add(new Entity(organization));
+            
+            return this;
+        }
+        public Vector<Entity> organization()
+        {
+            return get(Entity.Type.ORGANIZATION);
+        }
+        
+    };
+    
+}
