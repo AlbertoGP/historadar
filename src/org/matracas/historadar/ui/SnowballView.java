eadc1bd28d3647c85c69a28dbeb1ea6497c67256
Alberto Gonz?lez Palomo <Alberto.Gonzalez@matracas.org>
2010-03-24 00:08:24 +0100

    New component for the "snowball" document, where the user collects notes as he explores the documents.

---
+++ src/org/matracas/historadar/ui/SnowballView.java
@@ -0,0 +1,136 @@
+///////////////////////////////////////////////////////////////////////////
+//
+//   Copyright 2010 Alberto González Palomo
+//   Author: Alberto González Palomo - http://matracas.org/
+//
+//   This file is part of HistoRadar, the History Radar.
+//
+//   HistoRadar is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   HistoRadar is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with HistoRadar; if not, see <http://www.gnu.org/licenses/>.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.matracas.historadar.ui;
+
+import javax.swing.*;
+import java.awt.Font;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.StyleSheet;
+import javax.swing.text.Highlighter;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.prefs.Preferences;
+import java.util.regex.Pattern;
+
+import org.matracas.historadar.Document;
+
+/**
+ * Visualization of the work snowball.
+ */
+public class SnowballView extends DocumentView
+{
+    protected int contextRadius = 200;
+    
+    public SnowballView()
+    {
+        super();
+    }
+    
+    public SnowballView addEntry(Document document, DocumentView documentView, String searchQuery)
+    {
+        if (null == document) return this;
+        
+        int pageNumber      = documentView.getPageNumber();
+        int position        = documentView.getCaretPosition();
+        HTMLDocument htmlDocument = (HTMLDocument) documentView.getDocument();
+        String documentText;
+        try {
+            documentText = htmlDocument.getText(0, htmlDocument.getLength());
+        }
+        catch (javax.swing.text.BadLocationException e) {
+            // This isn't always accurate, but it's a last resort.
+            documentText = document.getPlainText();
+        }
+        if (position > documentText.length()) {
+            System.err.println("Error in SnowballView: caret position " + position + " > document length " + documentText.length() + "\n" + documentText);
+        }
+        
+        int contextBegin, contextEnd;
+        contextBegin = position;
+        contextEnd   = position + 1;
+        char c;
+        boolean minimumReached;
+        while (contextBegin > 0) {
+            minimumReached = position - contextBegin > contextRadius;
+            c = documentText.charAt(contextBegin);
+            if ('\n' == c || ':' == c ||
+                (!Character.isLetter(c) && minimumReached)) break;
+            --contextBegin;
+        }
+        while (contextEnd < documentText.length()) {
+            minimumReached = contextEnd - position > contextRadius;
+            c = documentText.charAt(contextEnd);
+            if ('\n' == c || ':' == c ||
+                (!Character.isLetter(c) && minimumReached)) break;
+            ++contextEnd;
+        }
+        String context = documentText.substring(contextBegin + 1, contextEnd);
+        java.util.regex.Matcher matcher = searchPattern(searchQuery).matcher(context);
+        context = matcher.replaceAll("<b>$0</b>");
+        
+        try {
+            HTMLDocument snowball = (HTMLDocument) getDocument();
+            javax.swing.text.html.HTMLEditorKit editorKit = (javax.swing.text.html.HTMLEditorKit) getEditorKit();
+            String content = "<div><h2>Document " + document.getIdentifier() + "</h2>";
+            content += "<table border='1' style='font-family:sans-serif'>";
+            Document.Metadata.Values values;
+            values = document.getMetadata().get(Document.Metadata.date);
+            if (values != null) {
+                java.util.Iterator<String> valueIterator = values.iterator();
+                while (valueIterator.hasNext()) {
+                    content += "<tr><td>Date</td><td colspan='2'>" + valueIterator.next() + "</td></tr>";
+                }
+            }
+            
+            content += "<tr><td>Search query</td><td>" + searchQuery + "</td>";
+            if (documentView.getMatchCount() > 0) {
+                content += "<td>" + (documentView.getCurrentMatch() + 1) + "</td></tr>";
+            }
+            else {
+                content += "<td>-</td></tr>";
+            }
+            content += "<tr><td>Position</td><td>" + position + "</td>";
+            content += "<td>" + (position * 100 / documentText.length()) + "%</td></tr>";
+            content += "<tr><td>Page</td><td colspan='2'>" + pageNumber + "</td></tr>";
+            java.util.Date date = new java.util.Date();
+            java.text.SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm");
+            content += "<tr><td>Date bookmarked</td><td colspan='2'>" + dateFormat.format(date) + "</td></tr>";
+            content += "<tr><td colspan='3' style='font-family:serif'>" + context + "</td></tr>";
+            content += "<tr><td>Notes</td><td colspan='2'>&nbsp;</td></tr>";
+            content += "</table></div> <br/>";
+            editorKit.insertHTML(snowball, snowball.getLength(),
+                                 content,
+                                 0, 0, null);
+            setCaretPosition(snowball.getLength());
+        }
+        catch (javax.swing.text.BadLocationException e) {
+            System.err.println("Error in SnowballView: " + e);
+        }
+        catch (java.io.IOException e) {
+            System.err.println("Error in SnowballView: " + e);
+        }
+        
+        return this;
+    }
+}
